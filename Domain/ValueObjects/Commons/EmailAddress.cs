d3eef67d7d7436538a488c4422f5b6a2e2433ab3
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-02 04:24:32 +0700

    feat<domain>: create `EmailAddress`

---
+++ Domain/ValueObjects/Commons/EmailAddress.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using Domain.Exceptions;
+
+namespace Domain.ValueObjects.Commons;
+
+public readonly partial struct EmailAddress
+{
+   private static readonly Regex EmailRegex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3}){1,})$");
+   
+   private readonly string Value { get; init; }
+
+   public EmailAddress(string value)
+   {
+      if (!EmailRegex.IsMatch(value))
+      {
+         throw new InvalidEmailAddressException();
+      }
+      Value = value;
+   }
+
+   public static explicit operator string(EmailAddress email)
+   {
+      return email.Value;
+   }
+}
