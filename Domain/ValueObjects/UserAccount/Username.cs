1e7b2e55e0c5734ab1a4e587e2ef08d7171cdf8e
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-02 04:35:31 +0700

    feat<domain> create `Username`

---
+++ Domain/ValueObjects/UserAccount/Username.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Exceptions;
+
+namespace Domain.ValueObjects.UserAccount;
+
+public readonly struct Username
+{
+   public static bool IsValid(string value)
+   {
+      return !string.IsNullOrEmpty(value);
+   }
+   
+   public string Value { get; init; }
+   
+   public Username(string value)
+   {
+      if (IsValid(value))
+      {
+         throw new InvalidUsernameException();
+      }
+      Value = value;
+   }
+}
