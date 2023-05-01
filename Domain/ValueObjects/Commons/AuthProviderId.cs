c23ff864fe89e0e6fb09b862f2d4811dde05092e
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-02 05:03:50 +0700

    feat<domain>: create `AuthProviderId`

---
+++ Domain/ValueObjects/Commons/AuthProviderId.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Exceptions;
+
+namespace Domain.ValueObjects.Commons;
+
+public readonly struct AuthProviderId
+{
+   public static bool IsValid(string value)
+   {
+      return !string.IsNullOrEmpty(value);
+   }
+   
+   public string Value { get; init; }
+
+   public AuthProviderId(string value)
+   {
+      if (!IsValid(value))
+      {
+         throw new InvalidAuthProviderIdException();
+      }
+      Value = value;
+   }
+
+   public static explicit operator string(AuthProviderId authProviderId)
+   {
+      return authProviderId.Value;
+   }
+
+   public static explicit operator AuthProviderId(string firebaseIdString)
+   {
+      return new AuthProviderId(firebaseIdString);
+   }
+}
