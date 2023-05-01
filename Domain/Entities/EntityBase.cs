c49a6d3e08fbf4d3e10d5bf1d59348c5420c6df7
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-02 03:47:22 +0700

    feat<domain>: create `EntityBase`

---
+++ Domain/Entities/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities;
+
+public abstract class EntityBase
+{
+   private protected EntityBase(Guid id)
+   {
+      Id = id;
+   }
+
+   public Guid Id { get; init; }
+
+   public DateTime InsertedAt { get; init; }
+   
+   public DateTime UpdatedAt { get; set; }
+}
