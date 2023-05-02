5a0c336f196dbf7cd5246c4d24fbe5ac3cd22345
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-02 15:55:18 +0700

    feat<infra>: config `PrimaryDbContext`

---
+++ Infrastructure/Config/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.PrimaryDb;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Config;
+
+public static class DependencyInjection
+{
+   public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+   {
+      services.AddPrimaryDb(configuration);
+   }
+
+   private static void AddPrimaryDb(this IServiceCollection services, IConfiguration configuration)
+   {
+      services.AddDbContext<PrimaryDbContext>(
+         options => options.UseNpgsql(configuration["PrimaryDbConnectionString"]));
+   }
+}
