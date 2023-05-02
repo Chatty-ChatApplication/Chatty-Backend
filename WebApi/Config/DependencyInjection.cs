62429560b6b5d9080a34c26630ef7d3b03a6b134
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-02 16:40:40 +0700

    feat<webapi>: add `ConfigServices`

---
+++ WebApi/Config/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Config;
+
+namespace WebApi.Config;
+
+public static class DependencyInjection
+{
+   public static void ConfigServices(this IServiceCollection services, IConfiguration configuration)
+   {
+      services.AddControllers();
+      services.AddEndpointsApiExplorer();
+      services.AddSwaggerGen();
+      
+      services.AddInfrastructure(configuration);
+   }
+}
