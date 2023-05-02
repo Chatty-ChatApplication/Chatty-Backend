dab392da6e8bf822d8c877f2f8f67941b2135556
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-02 16:24:10 +0700

    feat<infra>: override `OnConfiguring`

--- Infrastructure/PrimaryDb/PrimaryDbContext.cs
+++ Infrastructure/PrimaryDb/PrimaryDbContext.cs
@@ -1,14 +1,23 @@
 ﻿using Domain.Contracts.PrimaryDb;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure.PrimaryDb;
 
 public class PrimaryDbContext : DbContext, IPrimaryDbContext
 {
+   private readonly IConfiguration _configuration;
+   
    internal DbSet<UserAccount> UserAccounts { get; set; } = null!;
 
-   public PrimaryDbContext(DbContextOptions<PrimaryDbContext> options) : base(options)
+   public PrimaryDbContext(DbContextOptions<PrimaryDbContext> options, IConfiguration configuration) : base(options)
    {
+      _configuration = configuration;
+   }
+
+   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+   {
+      optionsBuilder.UseNpgsql(_configuration["PrimaryDbConnectionString"]);
    }
 }
