c94967f1838381fb25b90a856d287ab60506f880
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-02 15:54:44 +0700

    feat<infra>: create `PrimaryDbContext`

---
+++ Infrastructure/PrimaryDb/PrimaryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Contracts.PrimaryDb;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.PrimaryDb;
+
+public class PrimaryDbContext : DbContext, IPrimaryDbContext
+{
+   internal DbSet<UserAccount> UserAccounts { get; set; } = null!;
+
+   public PrimaryDbContext(DbContextOptions<PrimaryDbContext> options) : base(options)
+   {
+   }
+}
