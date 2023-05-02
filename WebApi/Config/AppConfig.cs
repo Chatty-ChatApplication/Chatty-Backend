0bf9fcc00b7781195c69090edf36a3179e52263a
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-02 16:40:58 +0700

    feat<webapi>: add app configuration `Config`

---
+++ WebApi/Config/AppConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApi.Config;
+
+public static class AppConfig
+{
+   public static void Config(this WebApplication app)
+   {
+      if (app.Environment.IsDevelopment())
+      {
+         app.UseSwagger();
+         app.UseSwaggerUI();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseAuthorization();
+      app.MapControllers();  
+   }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,21 +1,10 @@
+using WebApi.Config;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.ConfigServices(builder.Configuration);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-   app.UseSwagger();
-   app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.Config();
 app.Run();
